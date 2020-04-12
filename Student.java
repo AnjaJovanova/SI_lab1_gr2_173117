b3779e4244ff6bad0d983bfb113088f4348fd1a5
AnjaJovanova <anja.jovanova@gmail.com>
2020-04-12 15:12:14 +0200

    grades list added

---
+++ Student.java
@@ -0,0 +1,18 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+int grades[100];
+
+	//TODO constructor
+
+	//TODO seters & getters
+
+	public double getAverage() {
+		//TODO
+	}
+
+	public int ECTSCredits() {
+		//TODO
+	}
+}
