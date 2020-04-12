4b04a4f731690a4c9e2b5fe43d31ebcac7f6a3c7
AnjaJovanova <anja.jovanova@gmail.com>
2020-04-12 15:33:42 +0200

    students class finished

--- Student.java
+++ Student.java
@@ -2,14 +2,29 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-int grades[100];
+int grades[7];
 
 	//TODO constructor
+	Student(){
+	
+	}
 
 	//TODO seters & getters
 
+void setGrade(int x){
+for(int i=0;i<=7;i++)
+{
+    grades[i]=x;
+}
+ 	
+}
+void getGrade{
+     return grades[];
+}
 	public double getAverage() {
 		//TODO
+
+
 	}
 
 	public int ECTSCredits() {
